b1367c30153178fad88c17a262e207c7b9262e05
Mansoor Hussain <hussainmansoor876@gmail.com>
2020-02-29 22:38:50 +0500

    done

--- routes/subform.py
+++ routes/subform.py
@@ -160,6 +160,7 @@ def updateForm1():
     data['soldPrice'] = json.loads(data['soldPrice'])
     data['transactionFee'] = json.loads(data['transactionFee'])
     data['paidAmount'] = json.loads(data['paidAmount'])
+    data['zip'] = json.loads(data['zip'])
     fileData = request.files
     data['files'] = json.loads(data['files'])
     if(fileData):
