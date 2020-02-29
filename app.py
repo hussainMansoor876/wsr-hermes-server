d7e96ce2c4552c00e08ddd3dc7b4676cdf2e5aca
Mansoor Hussain <hussainmansoor876@gmail.com>
2020-03-01 01:06:06 +0500

    resolve server error

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ import os
 # load_dotenv()
 
 from flask_cors import CORS, cross_origin
-# from routes import login, subform, admin
+from routes import login, subform, admin
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = 'mongodb://mansoor:mansoor11@wsr-hermes-shard-00-00-cxem6.mongodb.net:27017,wsr-hermes-shard-00-01-cxem6.mongodb.net:27017,wsr-hermes-shard-00-02-cxem6.mongodb.net:27017/test?ssl=true&replicaSet=WSR-Hermes-shard-0&authSource=admin&retryWrites=true&w=majority'
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
 
 mongo = PyMongo(app)
@@ -21,9 +21,9 @@ mongo = PyMongo(app)
 CORS(app, allow_headers = ["Content-Type", "Authorization", "Access-Control-Allow-Credentials", "Access-Control-Allow-Origin"], supports_credentials=True)
 
 
-# app.register_blueprint(login.index_blueprint, url_prefix='/login')
-# app.register_blueprint(subform.index_blueprint, url_prefix='/subform')
-# app.register_blueprint(admin.index_blueprint, url_prefix='/admin')
+app.register_blueprint(login.index_blueprint, url_prefix='/login')
+app.register_blueprint(subform.index_blueprint, url_prefix='/subform')
+app.register_blueprint(admin.index_blueprint, url_prefix='/admin')
 
 @app.route('/')
 def index():
